34b0751dc9d46e168a092a830f6f63828acf631a
melanieaqui <melanie.wayuaquino@gmail.com>
2024-05-14 12:22:30 +0800

    put prime numbers in a list

--- Driver.java
+++ Driver.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 public class Driver {
     public static void main(String[] args) {
         /*TODO: 
-            1. Implement mutual exclusion for the `primes` list.
+            1.(Double check) Implement mutual exclusion for the `primes` list.
             2. Implement the computation of the runtime of your algorithm. 
                 - The timer starts after user input is collected, and the timer stops before printing the number of primes found.
             3. Double check?*/
@@ -18,6 +20,7 @@ public class Driver {
         //Implement threading to split the range of integers across the specified number of threads.
         int divisions= (limit-1)/nthreads + (limit-1)%nthreads;
         int lower =2;
+        Prime []threads = new Prime[nthreads];
         for(int i =0; i<nthreads;i++){
             if (lower+divisions-1>limit){
                  upper=limit;
@@ -26,38 +29,22 @@ public class Driver {
                 upper= lower+divisions-1;
 
             Prime p = new Prime(lower,upper) ;
+            threads[i]=p;
             p.start();
             lower = upper+1;
-            
-        }      
+            try{
+                threads[i].join();
+            }  catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+           
+        }   
+        for (int j =0; j< threads[nthreads-1].getList().size();j++){
+            System.out.println(threads[nthreads-1].getList().get(j));
+        } 
+        
 
     }
     
 }
 
-class Prime extends Thread{
-    int upper;
-    int lower;
-    public Prime(int lower, int upper){
-        this.upper= upper;
-        this.lower= lower;
-    }
-    public void run() {
-      for (int n =lower; n<=upper;n++){
-        if (isPrime(n))
-            System.out.println("Prime " + n); 
-        else
-            System.out.println("not Prime " + n);
-
-      }
-
-    }
-
-    public boolean isPrime(int n){
-        for (int i = 2; i < n; i++)
-            if (n % i == 0)
-                return false;
-        return true;
-    }
-
-}
---
+++ Prime.java
@@ -0,0 +1,47 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.ArrayList;
+
+
+public class Prime extends Thread{
+    private int upper;
+    private int lower;
+    private static ArrayList<Integer> PrimeList = new ArrayList<Integer>(); 
+    private static Lock lock = new ReentrantLock();
+
+    public Prime(int lower, int upper){
+        this.upper= upper;
+        this.lower= lower;
+    }
+    public void run() {
+     // lock.lock(); 
+      System.out.println("Thread " ); 
+      for (int n =lower; n<=upper;n++){
+        if (isPrime(n)){
+           // System.out.println("Prime " + n); 
+            addToList(n);
+        }
+        //else
+        //   System.out.println("not Prime " + n);
+
+      }
+     //lock.unlock();
+    }
+    public static synchronized void addToList(int n) {
+        lock.lock();
+        PrimeList.add(n);
+        lock.unlock();
+    }
+    public static synchronized ArrayList<Integer> getList() {
+        return PrimeList;
+    }
+
+
+    public boolean isPrime(int n){
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+        return true;
+    }
+
+}
